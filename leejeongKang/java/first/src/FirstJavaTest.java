29c8d653ac216d5e2e6fd4af36a2bef117c89d53
kangleejeong <krj1226@naver.com>
2021-12-22 16:43:24 +0900

    [강이정] test pr

--- leejeongKang/java/first/src/FirstJavaTest.java
+++ leejeongKang/java/first/src/FirstJavaTest.java
@@ -3,7 +3,6 @@ public class FirstJavaTest {
     public static void main(String[] args) {
         //sout
         System.out.println("hello world");
+        System.out.println("\n답답하다");
     }
 }
-
-
