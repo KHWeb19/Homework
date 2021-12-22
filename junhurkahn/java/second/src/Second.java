165e9cca88c1d6ee8789be7d21e7e71d98047a80
dkswnsgur <dkseho2135@naver.com>
2021-12-22 22:54:56 +0900

    [안준혁] 두 번쨰 코드

---
+++ junhurkahn/java/second/src/Second.java
@@ -0,0 +1,13 @@
+public class Second {
+    public static void main(String[] args) {
+        int num = 3;
+        float num2 = 3.14f; float num3 = 6.54f;
+        double num4 = 12.34; double num5 = 66.34; double num6 = 66.64;
+
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.printf("num4 : %f\nnum5 : %f\nnum6 : %f",num4,num5,num6);
+
+        }
+    }
