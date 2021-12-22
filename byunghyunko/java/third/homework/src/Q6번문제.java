1caa78a9261c369b60e130319265fbe74d815720
paran22 <paranstar22@gmail.com>
2021-12-23 04:56:56 +0900

    [고병현] 문제은행2 homework

---
+++ byunghyunko/java/third/homework/src/Q6번문제.java
@@ -0,0 +1,13 @@
+public class Q6번문제 {
+    public static void main(String[] args) {
+        // 6. 1 ~ 100까지 숫자중 3의 배수만 출력해보자!
+
+        int i = 1;
+        for (i = 1; i < 101; i++) {
+            if(i % 3 == 0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
---
+++ byunghyunko/java/third/homework/src/Q8번문제.java
@@ -0,0 +1,14 @@
+public class Q8번문제 {
+    public static void main(String[] args) {
+        // 8. 1 ~ 100까지 숫자를 순회한다.
+       // 2 ~ 10 사이의 랜덤한 숫자를 선택하고 이 숫자의 배수를 출력해보도록 한다.
+
+        int i = (int)(Math.random()*9+2);
+
+        for (int i2 = 1; i2 < 101; i2++) {
+            if (i2 % i == 0) {
+                System.out.println(i2);
+            }
+        }
+    }
+}
