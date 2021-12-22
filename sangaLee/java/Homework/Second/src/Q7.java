e89009381e2d19053d390139828066469edbf30d
asalwaysineedu <lso7182@naver.com>
2021-12-23 00:14:49 +0900

    [이상아] 12/22 Homework(1)

---
+++ sangaLee/java/Homework/Second/src/Q7.java
@@ -0,0 +1,16 @@
+public class Q7 {
+    public static void main(String[] args) {
+
+        // 주사위를 굴려서 짝수인 경우 당첨입니다! 출력
+
+        int rand = (int)(Math.random()*10+2);
+
+        if (rand % 2 == 0) {
+            System.out.println("당첨입니다!");
+        } else {
+            System.out.println(rand);
+        }
+
+
+    }
+}
---
+++ sangaLee/java/Homework/Second/src/Q9.java
@@ -0,0 +1,8 @@
+public class Q9 {
+    public static void main(String[] args) {
+
+        // 4~97까지의 랜덤 숫자를 생성해보세요.
+        System.out.println((int)(Math.random() * 94) + 4);
+
+    }
+}
