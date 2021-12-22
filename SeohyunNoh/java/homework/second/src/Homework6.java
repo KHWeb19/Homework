6e4e688a160576b7f6385f518da27fb02da3bb46
SeohyunNoh <sarahnoh7@naver.com>
2021-12-22 23:18:03 +0900

    [노서현] 문제은행2~11 Homework

---
+++ SeohyunNoh/java/homework/second/src/Homework6.java
@@ -0,0 +1,9 @@
+public class Homework6 {
+    public static void main(String[] args) {
+        int dice = (int) (Math.random() * 6 + 1);
+
+        if (dice % 2 == 0) {
+            System.out.println("당첨입니다!");
+        }
+    }
+}
