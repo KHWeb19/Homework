e68fcbcfe07cedf1f91e41c2dac0ebbfbb07234d
wns2034 <wns2034@gmail.com>
2021-12-22 22:13:40 +0900

    [박준혁] 문제은행 숙제

---
+++ JunHyeokPark/java/HomeWork/Second/src/problem4.java
@@ -0,0 +1,7 @@
+public class problem4 {
+    public static void main(String[] args) {
+        double rand1 = Math.random();
+        double rand2 = Math.random();
+        System.out.printf("rand1=%f, rand2=%f", rand1, rand2);
+    }
+}
