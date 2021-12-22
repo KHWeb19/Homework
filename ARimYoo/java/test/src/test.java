fa41345d19ed7353ce46afa33dcedc15cf631d55
rimirim <rimizzaang@naver.com>
2021-12-22 16:19:04 +0900

    [유아림] 문제은행 1번

---
+++ ARimYoo/java/test/src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        int num;
+
+        num = 3;
+
+        // println과 printf의 차이 :
+
+        System.out.println("숫자 = " + num);
+        System.out.printf("포맷 지정 숫자 = %d\n", num);
+    }
+}
