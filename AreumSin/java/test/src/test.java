030e1a6be81ec43137350dc12f17305aa766d808
sinareum <zhzkzhffk35@gmail.com>
2021-12-22 16:14:22 +0900

    [신아름] 테스트 코드 업로드

---
+++ AreumSin/java/test/src/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        // 변수 개념
+        int num;
+
+        num = 3;
+
+        System.out.println("숫자 = " + num);
+        System.out.printf("포맷 지정 숫자 = %d\n", num);
+    }
+}
