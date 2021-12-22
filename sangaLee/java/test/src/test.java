7699c271c116c020113903b00162e3d6a3eb12fc
asalwaysineedu <lso7182@naver.com>
2021-12-22 17:45:17 +0900

    [이상아] 문제은행1 PR

--- sangaLee/java/test/src/test.java
+++ sangaLee/java/test/src/test.java
@@ -1,10 +1,5 @@
-public class test {
+public class Test {
     public static void main(String[] args) {
-        int num;
-
-        num = 3;
-
-        System.out.println("숫자 = " + num);
-        System.out.printf("포맷 지정 숫자 = %d\n", num);
+        System.out.println("Test");
     }
 }
