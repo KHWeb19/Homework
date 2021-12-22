ab8028e5dfdcf65defa7837fc4befd449dca6810
flowraker <flowraker@gmail.com>
2021-12-22 22:33:18 +0900

    [이태호] 2일차 과제

---
+++ TaehoLee/JAVA/Homework/Second/src/Homework1.java
@@ -0,0 +1,13 @@
+public class Homework1 {
+    // 정수형 변수 1개, float형 변수 2개, double형 변수 3개에 아무 값이나 할당하고 출력합니다.
+
+    public static void main(String[] args) {
+        int num1 = 1, num2 =2;
+        float fnum1 = 1.1f, fnum2 = 2.2f;
+        double dnum1 = 1.1, dnum2 = 2.2, dnum3 = 3.3;
+
+        System.out.printf("정수형 변수 : %d, %d\n", num1, num2);
+        System.out.printf("float형 변수 : %f, %f\n",fnum1, fnum2);
+        System.out.printf("double형 변수 : %f, %f, %f\n", dnum1, dnum2, dnum3);
+    }
+}
---
+++ TaehoLee/JAVA/Homework/Second/src/Homework10.java
@@ -0,0 +1,15 @@
+public class Homework10 {
+    //65 ~ 122까지의 난수를 무작위로 생성하고 65 ~ 90 혹은 97 ~ 122에 해당하는 숫자만 출력해봅시다.
+
+    public static void main(String[] args) {
+        int num = (int)(Math.random() * 58) + 65;
+        boolean res;
+
+        if (res = num > 91 && num < 97){
+            ;
+        }
+            else{
+                System.out.println(num);
+            }
+    }
+}
