1902d6da3242b565a3077ab83ccf973771facf8b
sinareum <zhzkzhffk35@gmail.com>
2021-12-22 22:05:19 +0900

    [신아름] 첫 번째 과제 업로드

---
+++ AreumSin/java/homework/src/HomeworkB.java
@@ -0,0 +1,14 @@
+public class HomeworkB {
+    public static void main(String[] args) {
+
+        // Math.random() -> 0.0 ~ 0.999x
+        // 정수로 하면 0만 나옴
+        int num1 = (int)(Math.random());
+        int num2 = (int)(Math.random());
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+
+    }
+}
