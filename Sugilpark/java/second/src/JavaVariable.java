af200d4adc37004a0268b900758dcf6d7777e0aa
qkrtnrlf <dkslel217@naver.com>
2021-12-22 23:29:40 +0900

    [박수길]HomeWork

--- Sugilpark/java/second/src/JavaVariable.java
+++ Sugilpark/java/second/src/JavaVariable.java
@@ -1,5 +1,12 @@
 public class JavaVariable {
     public static void main(String[] args) {
+        //Java에서 변수를 만드는 방법
+        //데이터타입(자료형)을 적는다.
+        //변수 이름을 작성한다.
+        //필요하다면 = 3 등을 통해서 값을 할당한다.(있어도 그만 없어도 그만)
+
+        //데이터타입이란?
+        //정수형:정수형
         int num;
 
 
