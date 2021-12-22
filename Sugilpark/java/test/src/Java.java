db7ee5e5a844cc58aed5e583313f74fd701af804
qkrtnrlf <dkslel217@naver.com>
2021-12-22 16:21:51 +0900

    [박수길]문제은행1

---
+++ Sugilpark/java/test/src/Java.java
@@ -0,0 +1,13 @@
+public class Java {
+    public static void main(String[] args) {
+
+        int num;
+
+
+        num = 3;
+
+
+        System.out.println("숫자 = " + num);
+        System.out.printf("포맷 지정 숫자 = %d\n", num);
+    }
+}
