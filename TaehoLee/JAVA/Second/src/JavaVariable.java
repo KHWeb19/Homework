0cd533cc159fac90ef1257c11336bd89cad04030
flowraker <flowraker@gmail.com>
2021-12-22 16:21:30 +0900

    [이태호] PR 테스트

---
+++ TaehoLee/JAVA/Second/src/JavaVariable.java
@@ -0,0 +1,13 @@
+public class JavaVariable {
+    public static void main(String[] args) {
+        // 변수 num을 int로 지정
+        int num;
+        //변수 num의 값을 3으로 지정
+        num = 3;
+
+        // 시스템에 표시 "숫자 = " 과 num의 값
+        System.out.println("숫자 = " + num);
+        // 시스템에 표시
+        System.out.printf("포맷 지정 숫자 = %d\n" , num);
+    }
+}
