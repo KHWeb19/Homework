ac251e0fa04516bf4c96b3bf7b71c2dd98a4ad6e
ljieun <wldms8196@gmail.com>
2021-12-23 15:39:11 +0900

    [이지은] 문제은행[1] 질문추가

--- jieunLee/java/Homework/second/src/two.java
+++ jieunLee/java/Homework/second/src/two.java
@@ -6,6 +6,8 @@ public class two {
         double num4 = 5.112534556;
         double num5 = 5.55132556;
         double num6 = 5.895556;
-        System.out.printf("%d\n%f\n%f\n%f\n%f\n%f\n", num1, num2, num3, num4, num5, num6);
+        System.out.printf("%d\n%.2f\n%f\n%f\n%.8f\n%f\n", num1, num2, num3, num4, num5, num6);
     }
+
+    //소수점 여섯째짜리까지 표시가 고정인것 같다.
 }
