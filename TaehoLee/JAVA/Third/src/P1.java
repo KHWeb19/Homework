8fa951ae31550b5408a4e232b99e7aa4a60f3088
flowraker <flowraker@gmail.com>
2021-12-24 01:21:33 +0900

    [이태호] 두 번째 과제 업로드

--- TaehoLee/JAVA/Third/src/P1.java
+++ TaehoLee/JAVA/Third/src/P1.java
@@ -2,17 +2,18 @@ public class P1 {
     // 65 ~ 122 사이의 랜덤한 숫자 생성, 소문자나 대문자가 아니라면 다시 생성
 
     public static void main(String[] args) {
+        int min = 65, max = 122;
+        int rand;
+        boolean text = false;
 
-        int rand = (int)(Math.random() * 58) + 65;
+        while(!text){
 
-        if (rand <= 90 || rand >= 97){
+            rand = (int)(Math.random() * (max - min + 1)) + min;
+
+            if (rand <= 90 || rand >= 97){
             System.out.printf("문자 : %c", rand);
-        }
-        else{
-            while (rand > 90 && rand < 97) {
-                rand = (int)(Math.random() * 58) + 65;
+            text = true;
             }
-            System.out.printf("문자 : %c", rand);
         }
     }
 }
