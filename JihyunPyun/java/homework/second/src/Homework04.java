1e6a76608e137a4396e84bc33018a7cfb91fa21b
jihyun1101 <pjhh1180@naver.com>
2021-12-23 15:32:59 +0900

    [편지현] Homework1

---
+++ JihyunPyun/java/homework/second/src/Homework04.java
@@ -0,0 +1,8 @@
+public class Homework04 {
+    public static void main(String[] args) {
+        float rand1 = (float) Math.random();
+        float rand2 = (float) Math.random();
+        System.out.println("1번 랜덤값 : " + rand1);
+        System.out.println("2번 랜덤값 : " + rand2);
+    }
+}
