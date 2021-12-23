6a9200ab930576a05ff3846b5bb7a52c17a41c0d
Kcookie1 <930212@naver.com>
2021-12-23 12:26:59 +0900

    [김국현] 문제은행1 숙제

---
+++ KukhyunKim/java/homework/secend/src/test4.java
@@ -0,0 +1,11 @@
+public class test4 {
+    public static void main(String[] args) {
+        //float 변수 2개에 랜덤값 할당
+        float random1 = (float)(Math.random() + 1);
+        float random2 = (float)(Math.random() + 2);
+
+        //랜덤한 float변수 두개 출력
+        System.out.println("random1 = " + random1);
+        System.out.println("random2 = " + random2);
+    }
+}
