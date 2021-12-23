9a82812c8123853e0802fe7e33611d58dec3a183
thkim750 <thkim750@gmail.com>
2021-12-23 21:45:18 +0900

    [김태현] 문제은행2

---
+++ TaehyunKim/JAVA/third/src/HW02.java
@@ -0,0 +1,21 @@
+public class HW02 {
+    public static void main(String[] args) {
+        // 2. 배열을 사용하지 않고 1,1,2,3,5,8 피보나치 수열의 20번째 항을 구해보자
+
+        int fib_a = 1;
+        int fib_b = 1;
+
+        System.out.print("1, 1, ");
+        for (int i=0; i<20; i++){
+            int tmp = fib_a;
+            fib_a = fib_a + fib_b;
+            fib_b = tmp;
+            if (i != 19) {
+                System.out.printf("%d, ",fib_a);
+            }
+            else{
+                System.out.printf("%d",fib_a);
+            }
+        }
+    }
+}
