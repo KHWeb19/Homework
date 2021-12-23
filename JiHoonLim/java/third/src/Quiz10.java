2fdcf4b3d71ba9dac62cb39c85b49afa8a92c28f
JiHoonLim98 <lgh9758@naver.com>
2021-12-24 02:21:29 +0900

    [임지훈] 문제은행 [2] 10번 질문 수정

--- JiHoonLim/java/third/src/Quiz10.java
+++ JiHoonLim/java/third/src/Quiz10.java
@@ -2,6 +2,7 @@ public class Quiz10 {
     public static void main(String[] args) {
 
         int sum = 0;
+        int sum2 =0 ;
 
         for (int i = 1; i <= 100; i++) {
 
@@ -9,10 +10,12 @@ public class Quiz10 {
             System.out.println("랜덤으로 선택한 수는 " +rand);
 
             sum += rand;
-            System.out.println(i+"회차 이동거리는 " +sum);
+            sum2 += sum;
+            System.out.println(i+"회차 이동거리 " +sum);
             System.out.println();
 
-            /// 문제를 제대로 이해못했습니다
+            /// 문제를 제대로 이해한 건지 모르겠습니다.
         }
+        System.out.println("이동 시 나온 숫자들의 합 " +sum2);
     }
 }
