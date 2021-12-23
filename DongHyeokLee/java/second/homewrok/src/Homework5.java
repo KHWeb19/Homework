87a6b26d05c8e38af66ba1569304c2015bd0dfc6
dodongja <qs553@nate.com>
2021-12-23 11:37:06 +0900

    [이동혁] 2일차 숙제

---
+++ DongHyeokLee/java/second/homewrok/src/Homework5.java
@@ -0,0 +1,10 @@
+public class Homework5 {
+    public static void main(String[] args) {
+        double dnum1 = Math.random();
+        double dnum2 = (Math.random()*3.3);
+
+        System.out.println("dnum1 = " + dnum1);
+        System.out.println("dnum2 = " + dnum2);
+
+    }
+}
