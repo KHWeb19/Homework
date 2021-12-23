babc775d3913372034288aa976a04994bd33faf7
HoesungJung0507 <ghltjd159@naver.com>
2021-12-24 02:24:53 +0900

    [정회성]3일차 과제

---
+++ HoesungJung/java/third/homework/src/Q5.java
@@ -0,0 +1,13 @@
+public class Q5 {
+    public static void main(String[] args) {
+
+
+        int i = 1;
+        for (i = 1; i < 101; i++) {
+            if(i%2==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
