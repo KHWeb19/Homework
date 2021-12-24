b1cd32268c5b3c876f8ade8f3be5ced0fa01f512
JiHoonLim98 <lgh9758@naver.com>
2021-12-24 16:15:02 +0900

    [임지훈] 문제은행 [2]

--- JiHoonLim/java/third/src/Quiz2.java
+++ JiHoonLim/java/third/src/Quiz2.java
@@ -7,7 +7,7 @@ public class Quiz2 {
 
         System.out.println("1항은 " +j);
 
-        for (int k=1; k<=20; k++) {
+        for (int k=2; k<=20; k++) {
             sum = i+j;
             System.out.println(k+ "항은 " + sum);
             i = j;
