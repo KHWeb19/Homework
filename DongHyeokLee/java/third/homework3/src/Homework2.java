bd31ee3f71530a5ba2e6770386bd618bdbe13e8f
dodongja <qs553@nate.com>
2021-12-24 15:12:53 +0900

    [이동혁] 3일차 과제

---
+++ DongHyeokLee/java/third/homework3/src/Homework2.java
@@ -0,0 +1,37 @@
+public class Homework2 {
+    public static void main(String[] args) {
+        //1 ,1 , 2 , 3 ,5 ,8 ,13,21,34,55,89...
+        //일명 피보나치 수열의 20번째 항을 구하도록 프로그래밍
+        //어찌합니까~~
+
+        int MAX = 25;// Q. 이렇게 지정하면 몇번째 항까지 구할떄 이것만 변경해서 더 좋을라나?? 그럼 final 붙여야하나??
+        int sum ;
+        int i= 0;//0번쨰 항??
+        int j =1;//1번째 항
+
+
+        //20번 반복..
+        // 계속 노트에 끄적대서 찾아냈는데 개발자들도 답이 안나오면 노트에 끄적이는가?..
+        // 첫번째 항은 어떻게 하지..
+        System.out.println("피보나치 수열1번째 항 =" + j);
+
+        //for(int order = 2; order <= 20; order++){
+            for(int order = 2; order <= MAX; order++){
+
+                sum = i+j;
+                System.out.println("피보나치 수열" +  order  + "번째 항 =" + sum);
+
+                i = j;
+                j = sum;
+
+            }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ DongHyeokLee/java/third/homework3/src/Homework9.java
@@ -0,0 +1,38 @@
+public class Homework9 {
+    public static void main(String[] args) {
+        //1~100까지 숫자 순회
+        // 2~10 사이의 랜덤한 숫자 선택 이 숫자의 배수 출력
+        // 다음 루프에서 다시 랜덤 숫자 선택 해당 숫자 배수 출력
+        //그 다음 루프에서 다시 작업 반복
+        //끝까지 순회 했을때 출력된 숫자들의 합 => 끝까지??
+        //머선소리고...
+
+        final int MAX = 10, MIN = 2;
+        int range = MAX - MIN + 1;
+        //int ran = (int) (Math.random() * range + MIN);// 초기화 안 됌
+        int sum = 0;
+
+
+       //Q. 안나온 숫자들로만 계속 더해보고싶은데...
+        //Q.무한루프를 어떻게 끝내지
+        int i = 1;
+        while(true){
+            int ran = (int) (Math.random() * range + MIN);
+
+
+            while(i<=100){
+                if(i % ran == 0){
+                    System.out.println(ran + "의 배수 = " + i );
+                    sum += i;
+                }
+                i++;
+
+            }
+              i =1; // i 초기화하니 무한루프?함
+            System.out.println("지금 까지 출력된 숫자들의 합" + sum);
+        }
+
+
+
+    }
+}
