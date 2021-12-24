e162d9147694ff32301e5033929694653fa5e5e6
rimirim <rimizzaang@naver.com>
2021-12-24 13:55:40 +0900

    [유아림] Homework2

--- ARimYoo/java/homework/third/src/Homework1.java
+++ ARimYoo/java/homework/third/src/Homework1.java
@@ -1,25 +1,28 @@
 public class Homework1 {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            // 65 ~ 122 사이의 랜덤한 문자를 생성.
-            // 소문자나 대문자가 아니라면 다시 생성
+        // 65 ~ 122 사이의 랜덤한 문자를 생성.
+        // 소문자나 대문자가 아니라면 다시 생성
 
-            int min = 65;
-            int max = 122;
-            int range = max - min + 1;
+        int min = 65;
+        int max = 122;
+        int range = max - min + 1;
 
-            int rand = (int)(Math.random() * range + min);
+        int rand = (int) (Math.random() * range + min);
 
 
-            if (rand >= 65 && rand <=90 || rand >=97 && rand <=122){
+        if (rand >= 65 && rand <= 90 || rand >= 97 && rand <= 122) {
 
-                System.out.printf("rand = %c", rand);
+            System.out.printf("rand = %c", rand);
 
-            } if (rand >= 91 && rand <=96) {
+        } else {
 
-                rand = (int)(Math.random() * range + min);
-                System.out.printf("rand = %c", rand);
+            // 랜덤값이 91~96인 경우 계속해서 랜덤값을 구한다.
+            while (rand > 90 && rand < 97) {
+
+                rand = (int) (Math.random() * range + min);
 
             }
         }
     }
+}
