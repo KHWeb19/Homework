f925ee5f71b8d098a60d5c9d894cc91311ec9828
jihyun1101 <pjhh1180@naver.com>
2021-12-24 15:05:35 +0900

    [편지현] Homework2

---
+++ JihyunPyun/java/homework/third/src/Homework05.java
@@ -0,0 +1,10 @@
+public class Homework05 {
+    public static void main(String[] args) {
+        // 1 ~ 100까지 숫자중 짝수만 출력해보자.
+        for (int i = 1; i <= 100; i++){
+            if (i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
