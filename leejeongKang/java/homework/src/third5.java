501099c1c04facee7e2a0fc2f9a6883386a8222e
kangleejeong <krj1226@naver.com>
2021-12-24 15:36:35 +0900

    [강이정] 문제은행2

---
+++ leejeongKang/java/homework/src/third5.java
@@ -0,0 +1,10 @@
+public class third5 {
+    public static void main(String[] args) {
+        int i;
+        for (i=1; i<=100; i++){
+            if(i%2==0){
+                System.out.printf("짝수만 출력하시오 : %d\n", i);
+            }
+        }
+    }
+}
---
+++ leejeongKang/java/third/src/ForTest.java
@@ -0,0 +1,8 @@
+public class ForTest {
+    public static void main(String[] args) {
+        int i ;
+        for (i=1; i<=10; i++){
+            System.out.println("i = " +i);
+        }
+    }
+}
