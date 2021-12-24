ed9ff6ff8e5859379034cf7798a0d100f24ecae7
woosoojong <woo2886@naver.com>
2021-12-24 15:14:02 +0900

    [우수종] third homework

---
+++ SooJongWoo/java/homework/third/src/work4.java
@@ -0,0 +1,9 @@
+public class work4 {
+    public static void main(String[] args) {
+        // 구구단 7단을 출력해보자!
+        int dan = 7, num;
+        for (num = 1; num <= 9; num++) {
+            System.out.println(dan + "*" + num + "=" + dan * num);
+        }
+    }
+}
