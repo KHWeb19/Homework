62d3fb0b05e997d5cb1be44055d66686de10146b
ljieun <wldms8196@gmail.com>
2021-12-26 14:07:28 +0900

    [이지은] 문제은행 [2] 9번완전정복

--- jieunLee/java/Homework/third/src/HW05.java
+++ jieunLee/java/Homework/third/src/HW05.java
@@ -2,9 +2,11 @@ public class HW05 {
     public static void main(String[] args) {
         final int START = 1;
         final int END = 100;
+        final int DECISION = 2;
+        final int REMAIN = 0;
         System.out.printf("5. %d~%d까지 숫자중 짝수만 출력해보자!\n", START, END);
         for (int i=START; i<=END; i++) {
-            if (i%2==0) {
+            if (i%DECISION==REMAIN) {
                 System.out.println(i);
             }
         }
