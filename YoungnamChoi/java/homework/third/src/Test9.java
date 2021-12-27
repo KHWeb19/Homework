fde522ddff3225ea7a492e40350ba96855e9f67f
obasic <nuobasic@naver.com>
2021-12-27 15:24:26 +0900

    [최영남] 복습 homework

--- YoungnamChoi/java/homework/third/src/Test9.java
+++ YoungnamChoi/java/homework/third/src/Test9.java
@@ -6,20 +6,23 @@ public class Test9 {
         //    그 다음 루프에서 다시 작업을 반복한다.
         //    끝까지 순회 했을때 출력된 숫자들의 합은 얼마인가 ?
 
+
+        boolean num1 = true;
+        int dec =0;
         int sum = 0;
 
         for(int i =1; i<=100; i++){
-            int num = (int)(Math.random()*9+2);
-            System.out.println("선택된 값은" +num +"입니다.");
-            int sum2 = 0;
-            for(int j =1; j<101; j++){
-                if(j% num ==0){
-                    System.out.printf("%d " , j);
-                    sum2+= j;
-                }
+            while (num1){ //boolean이 있을 경우 while문을 돌고 랜덤값이 생겼을 경우 while문은 나와서 if문을 실행하게 된다.
+                dec = (int)(Math.random()*9+2); //랜덤값을 저장하는  변수 dec를 선언해서 랜덤값을 저장한다.
+                num1=false;
             }
-            System.out.printf("\n%d회차 결과값: %d\n", i, sum2);
-            sum += sum2;
+
+            if(i%dec == 0) {
+                System.out.printf("%d의 배수 i = %d\n " , dec, i); //난수의 배수를 선택해서 하나씩 더해주는 것이다.
+                num1=true;
+                sum += i;
+            }
+
         }
         System.out.println("최종 결과값: " + sum);
     }
