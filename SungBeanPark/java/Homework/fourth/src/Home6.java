85709ec5f01f4191f324df300143fab191809bb4
Mong9 <bean326@naver.com>
2021-12-27 15:37:51 +0900

    [박성빈] 복습

---
+++ SungBeanPark/java/Homework/fourth/src/Home6.java
@@ -0,0 +1,10 @@
+public class Home6 {
+    public static void main(String[] args) {
+        //1 ~ 100까지 숫자중 3의 배수만 출력해보자!
+        //for문을 사용하여 1~ 100까지 돌린다.
+        //if문을 사용하여 i % 3 = 0 으로하여 3의 배수들만 출력해준다.
+        //for문은 for(i = START; i <= END; i++);해준다.
+        //START = 1 END = 100, if문의 3의 변수를 a로 나머지 0 을 decision=0으로 만들어줌.
+        //출력
+    }
+}
