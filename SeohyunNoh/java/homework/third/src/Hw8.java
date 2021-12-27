bb9f78887283725b039315842e025a92f1ebba6b
SeohyunNoh <sarahnoh7@naver.com>
2021-12-27 19:34:45 +0900

    [노서현] 문제은행2 복습

--- SeohyunNoh/java/homework/third/src/Hw8.java
+++ SeohyunNoh/java/homework/third/src/Hw8.java
@@ -9,8 +9,7 @@ public class Hw8 {
         //1~100까지 숫자 순회
         for(int i=1 ; i<=100 ; i++){
             if(i % rand == 0){
-                // 출력하면 가로로 rand의 배수 : ~~~ 가 나오는게 아니라
-                // 세로로 배수들이 출력된다. 어떻게 가로로하는지 모르겠음
+
                 System.out.println(rand + "의 배수 : " + i);
             }
         }
