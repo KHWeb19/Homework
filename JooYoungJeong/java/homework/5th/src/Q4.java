586f4406467e745832a672a62eefaa83a2b4e7d1
GotDol2Ah <GotDol2Ah@gmail.com>
2021-12-28 16:13:55 +0900

    [정주영] 12/27 Homework3

---
+++ JooYoungJeong/java/homework/5th/src/Q4.java
@@ -0,0 +1,25 @@
+public class Q4 {
+    public static void main(String[] args) {
+
+        // 반 학생이 30명이 있다.
+        // 이들은 모두 시험을 치뤘고 모든 학생들은 60점 미만이 없다고 한다.
+        // 이 상태에서 학생들의 점수를 임의로 배치하고
+        // 학급의 평균값을 구해보도록 한다.
+
+        final int MAX = 100;
+        final int MIN = 60;
+        int RANGE = MAX - MIN +1;
+
+        int range = MAX - MIN -1;
+        int[] arr = new int[RANGE];
+
+        for (int i = 0; i< RANGE; i++){
+
+
+        }
+
+
+
+
+        }
+}
---
+++ JooYoungJeong/java/homework/5th/src/Q6.java
@@ -0,0 +1,13 @@
+public class Q6 {
+    public static void main(String[] args) {
+
+        // 45678911234라는 숫자를 BigInteger에 배치한다.
+        // 각 자리수에 맞는 숫자를 배열에 배치하도록 한다.
+        // ex) 1234 = 1 x 10^3 + 2 x 10^2 + 3 x 10^1 + 4 x 10^0
+        // arr[0] = 4, arr[1] = 3, arr[2] = 2, arr[3] = 1
+
+
+
+        }
+
+    }
