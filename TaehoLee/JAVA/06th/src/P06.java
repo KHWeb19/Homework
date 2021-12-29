9df46975990c1b2b3456e5a71b062ffc8aeb60ea
flowraker <flowraker@gmail.com>
2021-12-29 15:20:11 +0900

    [이태호] 6일차 과제 ---- 5일차 과제 복습

---
+++ TaehoLee/JAVA/06th/src/P06.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class P06 {
+    //45678911234라는 숫자를 BigInteger에 배치
+    // 각 자리에 맞는 숫자를 배열에 배치
+    public static void main(String[] args) {
+
+        BigInteger num = new BigInteger("45678911234");
+
+        // 조금더 고민 필요!!
+
+    }
+}
