b250f6430ccf7cade5bb9fc71e5fd6a35d70d3d3
kangleejeong <krj1226@naver.com>
2022-01-04 05:38:23 +0900

    [강이정] 문제은행 4

--- leejeongKang/java/9th/src/student.java
+++ leejeongKang/java/9th/src/student.java
@@ -5,14 +5,16 @@ public class student {
     public int grade;
     //학생 기본정보
 
-    static int subjectNum = 3;                                  // 과목 수
     public final int MAX = 100;                                 // 점수의 최댓값과 최솟값
     public final int MIN = 0;
 
-    String[] subject = new String[]{"국어", "영어", "수학"};      ///과목 이름 문자 배열
-    static int[] score = new int[subjectNum];                  /// 과목의 점수 배열
-    boolean isTrue = true;
 
+    static String[] subject = new String[]{"국어", "영어", "수학"};      ///과목 이름 문자 배열
+    static int subjectNum = subject.length;                                 ////과목 수
+    ///수정-> final int subjectNum에 수를 넣지 않고 스트링.length로 바꿈
+
+    static int[] score = new int[subjectNum];                                /// 과목의 점수 배열
+    boolean isTrue = true;
 
     public void InputScore() {
         for (int i = 0;i <subjectNum; i++) {
