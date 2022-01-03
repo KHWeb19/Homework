5b4a862c17453ee6623d486836e95e5c13ca4604
sknyuki <sknyuki009@gmail.com>
2022-01-03 14:48:36 +0900

    [세키네] 문제은행4

--- sknyuki/java/HomeWork/6th/src/M10.java
+++
@@ -1,8 +0,0 @@
-public class M10 {
-    public static void main(String[] args) {
-        Variance varscore=new Variance();
-        Deviation devscore=new Deviation();
-        varscore.initVariancescore();
-        devscore.initDeviationScore();
-    }
-}
