2b2aaac2064ada8cf268f228c3cf1378f98cbe67
asalwaysineedu <lso7182@naver.com>
2022-01-04 14:57:41 +0900

    [이상아] 문제은행 4 초보자용

---
+++ sangaLee/java/Homework/9th/src/BeginnerQ1.java
@@ -0,0 +1,12 @@
+public class BeginnerQ1 {
+    public static void main(String[] args) {
+
+        // Q1. 2 by 2 이중 배열을 초기화해서 아무 값이나 넣어보세요.
+
+        int[][] array = new int[2][2]; // 정수를 4개씩 담을 수 있는 배열이 2개 생성된다.
+
+        // int[][] arr = {{1,2} ,{3,4}};
+
+
+    }
+}
