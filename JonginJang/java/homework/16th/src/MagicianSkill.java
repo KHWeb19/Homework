95212c9a7a18382bf24c0c6b569635aa72bcdfa3
gafgc <whddls3210@naver.com>
2022-01-12 15:54:51 +0900

    [장종인] 0111_과제_16th

---
+++ JonginJang/java/homework/16th/src/MagicianSkill.java
@@ -0,0 +1,11 @@
+public class MagicianSkill implements Skill{
+    @Override
+    public String setQSkill() {
+        return "Q스킬:불태우기";
+    }
+
+    @Override
+    public String setWSkill() {
+        return "W스킬:얼리기";
+    }
+}
