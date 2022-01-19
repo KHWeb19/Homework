481840af01aa8ed06199f634405b5bae59c02d27
jiyeon12345 <wldys5353@naver.com>
2022-01-19 20:59:11 +0900

    [인지연] 2022.1.21 숙제 - 문제은행6 상속시키기

--- jiyeonIn/java/Homework/21th/src/SquaredAndSum2.java
+++ jiyeonIn/java/Homework/21th/src/SquaredAndSum2.java
@@ -1,19 +1,20 @@
-public class SquaredAndSum2 extends XYvalue implements Runnable {
+public class SquaredAndSum2 extends Xvalue implements Runnable {
     final static float BASIC = 0.001f;
     private float squaredStanderd2;
     private static int count;
-    private XYvalue[] xy;
-    //x와 y를 나눠서 값을 도출 후, 그 값을 곱하는 형식으로 만들어보고 싶음.
+    private Xvalue[] xy;
+    //x1과 x2를 나눠서 값을 도출 후, 그 값을 곱하는 형식으로 만들어보고 싶음.
     //이렇게 만들어보고 싶은 이유는 0.001이 critical section이 되어서 lock 을 만들어보고 싶음.(그럼, +0.001 전에 lock을 걸어야한다.)
-    // -> x,y이중배열 만들고 싶긴한데. 그러면 critical section을 쓸 수 없다.
-    //x배열, y배열, 그걸 곱한 값의 배열 총 3개의 배열을 만들어야할까? -> 이건 답이 아니였음. 이렇게 되면 여기서도 반복문을 쓰니까 3000 * 3000번이 반복 되는꼴.
+    // -> x1,x2이중배열 만들고 싶긴한데. 그러면 critical section을 쓸 수 없다.
+    //x1배열, x2배열, 그걸 곱한 값의 배열 총 3개의 배열을 만들어야할까? -> 이건 답이 아니였음. 이렇게 되면 여기서도 반복문을 쓰니까 3000 * 3000번이 반복 되는꼴.
     // --> 객체배열을 썼기 때문이다. 어레이리스트로도 만들어보고싶었는데 add 를 3000번 할 자신이 없다. 혹시 add를 안하고 for문으로도 값을 입력할 수 있나요?
 
+    // y = x^2인데 잘못생각해서 x * y 할뻔했음.
 
     public SquaredAndSum2(int squaredStanderd2) {
         this.squaredStanderd2 = squaredStanderd2;
         count = (int) (squaredStanderd2 / BASIC) + 1; // 3천번 반복하는 카운트 체크 완료 /
-        xy = new XYvalue[count];
+        xy = new Xvalue[count];
 
     }
 
@@ -25,7 +26,7 @@ public class SquaredAndSum2 extends XYvalue implements Runnable {
     public void run() {
         try {
             for(int i=0 ; i < count ; i++){
-                System.out.printf("x = %.4f, y= %.4f, 값 = %.4f\n" , xlineValue(), ylineValue(), multyValue());
+                System.out.printf("x1 = %.4f, x2= %.4f, y값 = %.4f\n" , x1lineValue(), x2lineValue(), multyValue());
                 Thread.sleep(5);//결과 값 8.9998, 도출완료
             }
         } catch (InterruptedException e) {
---
+++ jiyeonIn/java/Homework/23th/src/Bank6Prob2.java
@@ -0,0 +1,28 @@
+public class Bank6Prob2 {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadQuadraticEquation.calcEachThreadTotal(0, 3);
+
+        ThreadQuadraticEquation[] rect = new ThreadQuadraticEquation[ThreadQuadraticEquation.THREAD_MAX];
+
+        for (int i = 0; i < ThreadQuadraticEquation.THREAD_MAX; i++)
+        {
+            rect[i] = new ThreadQuadraticEquation();
+        }
+
+        for (int i = 0; i < ThreadQuadraticEquation.THREAD_MAX; i++)
+        {
+            rect[i].start();
+            rect[i].join();
+        }
+
+        System.out.printf("%d개의 스레드가 모든 작업을 완료하였습니다.", ThreadQuadraticEquation.THREAD_MAX);
+
+        float finalResult = 0;
+
+        for (int i = 0; i < ThreadRectangle.THREAD_MAX; i++) {
+            finalResult += rect[i].getSum();
+        }
+
+        System.out.println("최종 결과는 ? " + finalResult);
+    }
+}
