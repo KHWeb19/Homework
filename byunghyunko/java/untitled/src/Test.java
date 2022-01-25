1c39d6147adcc7d6a63bc6ac74416b555f012de7
paran22 <paranstar22@gmail.com>
2022-01-25 10:59:15 +0900

    [고병현] 25th 과제

---
+++ byunghyunko/java/untitled/src/Test.java
@@ -0,0 +1,68 @@
+public class Test {
+    public static void main(String[] args) {
+
+        //예제2-1
+        System.out.println("Hellow, world");
+        System.out.print("Hellow");
+        System.out.println("world");
+
+        System.out.println("========================");
+
+        //예제2-2
+        System.out.println("Hellow, world");
+        System.out.print("3+5=");
+        System.out.println(3+5);
+
+        System.out.println("========================");
+
+        //예제2-3
+        System.out.println(5+3);
+        System.out.println(5-3);
+        System.out.println(5*3);
+        System.out.println(5/3);
+
+
+        System.out.println("========================");
+
+
+        //예제2-4
+        int x4 = 5;
+        System.out.println(x4);
+
+        x4 = 10;
+        System.out.println(x4);
+
+
+        System.out.println("========================");
+
+
+        //예제2-5
+        int x5 = 5;
+        int y5 = 3;
+        System.out.println(x5+y5);
+        System.out.println(x5-y5);
+        System.out.println(x5*y5);
+        System.out.println(x5/y5);
+
+
+        System.out.println("========================");
+
+
+        //예제2-6
+        int x6 = 100;
+        double y6 = 3.14;
+        char ch = 'a';
+        string str = "abc";
+
+        System.out.println(x6);
+        System.out.println(y6);
+        System.out.println(ch);
+        System.out.println(str);
+
+
+
+
+
+
+    }
+}
