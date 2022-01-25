cae0fee2fd26b4a2f4c94578650de63941301e44
jiyeon12345 <wldys5353@naver.com>
2022-01-25 20:40:54 +0900

    [인지연] 2022.1.25 문제은행 7_2 다시 풀어보기

--- jiyeonIn/spring/Homework/demo/src/main/java/com/example/demo/DemoApplication.java
+++ jiyeonIn/spring/Homework/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.utility.thread.ThreadManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		ThreadManager tm = new ThreadManager();
+		tm.start();
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- jiyeonIn/spring/Homework/demo/src/main/java/com/example/demo/utility/thread/ThreadManager.java
+++ jiyeonIn/spring/Homework/demo/src/main/java/com/example/demo/utility/thread/ThreadManager.java
@@ -1,19 +1,24 @@
-package com.example.demo.controller25thHW;
+package com.example.demo.utility.thread;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadManager extends Thread {
 
-    static int test;
-    static Lock lock;
+    private static int test;
+    private Lock lock;
 
     public ThreadManager()  {
         test = 0;
         lock = new ReentrantLock();
     }
 
-    public static void plusOne() throws InterruptedException {
+    public static int getTest(){
+        return test;
+    }
+
+
+    public void plusOne() throws InterruptedException {
         boolean addOne = true;
         while (addOne){
             test++;
@@ -21,10 +26,8 @@ public class ThreadManager extends Thread {
             if(test>100){
                 addOne = false;
             }
-            System.out.println(test);
         }
     }
-
 
     @Override
     public void run() {
@@ -44,9 +47,8 @@ public class ThreadManager extends Thread {
 //그리고 새로고침 할때마다 증가하는 그 값을 출력하도록 한다.(??)
 //우선 consol창에는 3초마다 한번씩 값이 잘 나옴. 근데 그거를 웹 상에 출력하는거를 못하겠음
 
-run()을 넣었을때는 0만 출력(콘솔창 안에서는 1씩 증가하고있음),
-start()를 하면 아예 오류가 난다.
-어떻게 만들어야할까? 혹시 몰라서 test++하는 곳을 static으로 만들었는데도 따로 변동없음.
-그곳을 return형으로 만들어도 안됨...test의 증가하는 값을 웹 상에 업데잇 하는 방법이 뭐가 있을까
+run()을 넣었을때는 0이 출력, start()를 하면 아예 오류가 난다.
+어떻게 만들어야할까? 혹시 몰라서 test++하는 곳을 static으로 만들었는데도 답이 아닌것같음.
+그곳을 return형으로 만들어도 안됨...test를 업데잇 하는 방법이 뭐가 있을까
  */
 
