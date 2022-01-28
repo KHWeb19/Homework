7c21ca2da2a3bfe3112ee20d2624bba2383adb76
ljieun <wldms8196@gmail.com>
2022-01-28 19:36:37 +0900

    [이지은] 29th 복습

---
+++ jieunLee/spring/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 서버: 스프링부트
+// 클라이언트: url 요청
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
