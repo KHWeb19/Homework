e1a5a223faff22f2bba2a8bfe4a8d346da94ffbf
jiyeon12345 <wldys5353@naver.com>
2022-01-28 14:18:49 +0900

    [인지연] 2022.1.27 문제은행 8 복습

--- jiyeonIn/spring/Homework/demo/src/main/java/com/example/demo/DemoApplication.java
+++ jiyeonIn/spring/Homework/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.utility.nomal.Student;
-import com.example.demo.utility.thread.GeometricProgression;
 import com.example.demo.utility.thread.ThreadManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +11,6 @@ public class DemoApplication {
 		ThreadManager tm = new ThreadManager();
 		tm.start();
 
-		GeometricProgression gp = new GeometricProgression();
-		gp.start();
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
