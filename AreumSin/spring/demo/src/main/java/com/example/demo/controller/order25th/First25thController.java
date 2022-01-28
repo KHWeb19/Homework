26b2ecc96931f0f7b68a044fa8004a052f166bcc
sinareum <kuuniin@gmail.com>
2022-01-28 19:47:51 +0900

    29일차 수업 업로드

--- AreumSin/spring/demo/src/main/java/com/example/demo/controller/order25th/First25thController.java
+++ AreumSin/spring/demo/src/main/java/com/example/demo/controller/order25th/First25thController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.controller.order25th;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- AreumSin/spring/demo/src/main/java/com/example/demo/controller/order28th/Bank8Dash28thController.java
+++ AreumSin/spring/demo/src/main/java/com/example/demo/controller/order28th/Bank8Dash28thController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.controller.order28th;
 
 import com.example.demo.utility.Dice;
 import com.example.demo.utility.school.SchoolClass;
