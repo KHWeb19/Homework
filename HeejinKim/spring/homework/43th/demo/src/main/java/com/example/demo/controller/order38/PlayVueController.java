c613d18f0c83a58166435186c4d880d9caaf22e0
legojinK <ckgksck@gmail.com>
2022-02-25 01:45:02 +0900

    [김희진] js 복습 (1)

---
+++ HeejinKim/spring/homework/43th/demo/src/main/java/com/example/demo/controller/order38/PlayVueController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller.order38;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/play/ncvue")
+public class PlayVueController {
+
+    @GetMapping("/vue-index")
+    public String index () {
+        log.info("index()");
+
+        return "/38th/ncvue/index";
+    }
+
+    @GetMapping("/dataType")
+    public String dataType(){
+        log.info("dataType()");
+
+        return "/38th/ncvue/dataType";
+    }
+
+    @GetMapping("/jsDataType")
+    public String jsDataType(){
+        log.info("jsDataType()");
+
+        return "/38th/ncvue/jsDataType";
+    }
+
+
+
+}
+
