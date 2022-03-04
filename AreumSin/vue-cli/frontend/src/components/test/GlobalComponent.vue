b0d74c70634297882f32e6266047b44fd68ad021
sinareum <kuuniin@gmail.com>
2022-03-04 21:58:39 +0900

    48일차 업로드

---
+++ AreumSin/vue-cli/frontend/src/components/test/GlobalComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="addCounter">{{ counter }}</button> <!-- 어떤 메서드가 적용될지 지정함. -->
+</template>
+
+<!-- 기능별로 vue 를 분리함. -->
+
+<script>
+export default {
+  name: "global-component",
+  props: ['initialTest'],
+  data(){
+    return {
+      counter: this.initialTest // props랑 연결이 되서 counter을 맵핑시킨것.
+      // 왜? v-bind를 사용해서
+    }
+  },
+  methods: {
+    addCounter (){
+      this.counter += 1;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
