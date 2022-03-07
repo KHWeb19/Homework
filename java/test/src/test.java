c8ab954c27a7a2e37da2f8022eec7f729804d78a
sknyuki <sknyuki009@gmail.com>
2022-03-07 12:19:28 +0900

    [세키네]개인 복습 노트

--- java/test/src/test.java
+++ java/test/src/test.java
@@ -1,5 +1,10 @@
-public class test {
+public class Test {
     public static void main(String[] args) {
-        System.out.println("test");
+        int num;
+
+        num = 3;
+
+        System.out.println("숫자 = " + num);
+        System.out.printf("포맷 지정 숫자 = %d\n", num);
     }
 }
