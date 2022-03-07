9e2910b23562add04d9b1c3e5ba887e652eabd83
JiHoonLim98 <lgh9758@naver.com>
2022-03-08 02:58:14 +0900

    [임지훈] 48일차

---
+++ JiHoonLim/vuecli/frontend/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <div id="header">
+      <router-link
+        :to="{ name: 'Home' }"
+        class="nav-link"
+        active-class="active"
+      >
+        Home
+      </router-link>
+      <router-link
+        :to="{ name: 'Test' }"
+        class="nav-link"
+        active-class="active"
+      >
+        Test
+      </router-link>
+      <router-link
+        :to="{ name: 'BoardListPage' }"
+        class="nav-link"
+        active-class="active"
+      >
+        BoardListPage
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
