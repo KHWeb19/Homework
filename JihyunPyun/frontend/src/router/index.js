f209a6bdf19c8d27cd653549443cc77630a6cae5
jihyun1101 <pjhh1180@naver.com>
2022-03-08 23:06:18 +0900

    [편지현] 48th 게시판 복습

---
+++ JihyunPyun/frontend/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Test from '@/views/Test.vue'
+
+import BoardListPage from '@/views/board/BoardListPage.vue'
+import BoardRegisterPage from '@/views/board/BoardRegisterPage.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
+    },
+    {
+      path: '/boardList',
+      name: 'BoardListPage',
+      component: BoardListPage
+    },
+    {
+      path: '/boardRegister',
+      name: 'BoardRegisterPage',
+      component: BoardRegisterPage
+    }
+  ]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ JihyunPyun/frontend/src/views/board/BoardListPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="board">
+        <h2>Vue + Spring 게시판</h2>
+        <router-link :to="{ name: 'BoardRegisterPage' }">
+            게시물 작성
+        </router-link>
+        <board-list :boards="boards"/>
+    </div>
+</template>
+
+<script>
+
+import BoardList from '@/components/board/BoardList.vue'
+import { mapState, mapActions } from 'vuex'
+
+export default {
+    name: 'BoardListPage',
+    components: {
+        BoardList
+    },
+    computed: {
+        ...mapState(['boards']) //이름 그대로 state와 연결
+    },
+    mounted () {
+        this.fetchBoardList()
+    },
+    methods: {
+        ...mapActions(['fetchBoardList']) 
+    }
+}
+
+</script>
