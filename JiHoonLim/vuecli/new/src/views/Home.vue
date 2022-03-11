0c5e3bbf13bdca8b54fac0a2b00321d16d375176
JiHoonLim98 <lgh9758@naver.com>
2022-03-11 21:27:49 +0900

    [임지훈] EventBus 복습

---
+++ JiHoonLim/vuecli/new/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <div id="header">
+      <router-link style="text-decoration: none" :to="{ name: 'Home' }">
+        &nbsp;Home&nbsp;
+      </router-link>
+      <router-link style="text-decoration: none" :to="{ name: 'Test' }">
+        &nbsp;Test&nbsp;
+      </router-link>
+      <router-link
+        style="text-decoration: none"
+        :to="{ name: 'BoardListPage' }"
+      >
+        &nbsp;BoardListPage&nbsp;
+      </router-link>
+      <router-link style="text-decoration: none" :to="{ name: 'Concave' }">
+        &nbsp;이것이 삼목이닷!&nbsp;
+      </router-link>
+      <router-link
+        style="text-decoration: none"
+        :to="{ name: 'EventBusTestPage' }"
+      >
+        &nbsp;이것이 EventBusTestPage닷!&nbsp;
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
