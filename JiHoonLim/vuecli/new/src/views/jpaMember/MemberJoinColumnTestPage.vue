9c711fc3c141d8931588f475656ef47b9f20ad4d
JiHoonLim98 <lgh9758@naver.com>
2022-03-22 00:06:25 +0900

    [임지훈] JPA 회원가입 복습

---
+++ JiHoonLim/vuecli/new/src/views/jpaMember/MemberJoinColumnTestPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div align="center">
+      <h2>회원가입</h2>
+    </div>
+
+    <member-join-column-test-form @submit="onSubmit" />
+  </div>
+</template>
+
+<script>
+import MemberJoinColumnTestForm from "@/components/jpaMember/MemberJoinColumnTestForm.vue";
+import axios from "axios";
+
+export default {
+  name: "MemberJoinColumnTestPage",
+  components: {
+    MemberJoinColumnTestForm,
+  },
+  methods: {
+    onSubmit(payload) {
+      const { id, pw, auth } = payload;
+      axios
+        .post("http://localhost:7777/vueJpaMemberAuth/register", {
+          id,
+          pw,
+          auth,
+        })
+        .then((res) => {
+          alert("등록성공" + res);
+        })
+        .catch((res) => {
+          alert(res.response.data.message);
+        });
+    },
+  },
+};
+</script>
---
+++ JiHoonLim/vuecli/new/src/views/jpaMember/MemberRegisterPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div align="center">
+      <h2>회원 가입</h2>
+    </div>
+
+    <member-register-form @submit="onSubmit"/>
+  </div>
+</template>
+
+<script>
+
+import MemberRegisterForm from '@/components/jpaMember/MemberRegisterForm.vue'
+import axios from 'axios'
+
+export default {
+  name: "MemberRegisterPage.vue",
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    MemberRegisterForm
+  },
+  methods: {
+    onSubmit (payload) {
+      const { id, pw } = payload
+      axios.post('http://localhost:7777/vueJpaMember/register', { id, pw })
+        .then(res => {
+          alert('등록 성공! - ' + res)
+        })
+        .catch(res => {
+          alert(res.response.data.message)
+        })
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
