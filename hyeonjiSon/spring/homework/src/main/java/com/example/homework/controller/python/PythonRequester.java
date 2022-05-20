7804c21f2d0af01c5c48b048b925401f3ee4cc57
HyeonJi-Son <sustka46@gmail.com>
2022-05-20 19:22:24 +0900

    Revert "[손현지] python 강의 [5일차]"
    This reverts commit 62a4ed855747d61172a896b95f70b6e2d4e750d3.

--- hyeonjiSon/spring/homework/src/main/java/com/example/homework/controller/python/PythonRequester.java
+++ hyeonjiSon/spring/homework/src/main/java/com/example/homework/controller/python/PythonRequester.java
@@ -1,6 +1,5 @@
 package com.example.homework.controller.python;
 
-import com.example.homework.entity.python.PythonProduct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -47,58 +46,4 @@ public class PythonRequester {
 
         return modelAndView;
     }
-
-    @GetMapping("/spring2python-multi")
-    public ModelAndView spring2PythonMulti(Model model) {
-        log.info("spring2PythonMulti()");
-
-        List<HttpMessageConverter<?>> converters = new ArrayList<>();
-
-        converters.add(new FormHttpMessageConverter());
-        converters.add(new StringHttpMessageConverter());
-
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setMessageConverters(converters);
-
-        String result = restTemplate.getForObject(
-                "http://localhost:5000/python-request-multi",
-                String.class
-        );
-
-        log.info("result = " + result);
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("python/pyResult");
-
-        model.addAttribute("resultMsg", result);
-
-        return modelAndView;
-    }
-
-    @GetMapping("/spring2python-realdata")
-    public ModelAndView spring2PythonRealData(Model model) {
-        log.info("spring2PythonRealData()");
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        PythonProduct pythonProduct = new PythonProduct(35000L);
-
-        String result = restTemplate.postForObject(
-                "http://localhost:5000/python-request-realdata",
-                pythonProduct,
-                String.class
-        );
-
-        log.info("request success!");
-
-        log.info("result = " + result);
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("python/pyResult");
-
-        model.addAttribute("resultMsg", result);
-
-        return modelAndView;
-    }
-
 }
