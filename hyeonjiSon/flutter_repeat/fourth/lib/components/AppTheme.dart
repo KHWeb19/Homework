2dd070d9f5e98b56bfafb5dfcc376dfb5df2f6fe
HyeonJi-Son <sustka46@gmail.com>
2022-06-08 18:07:17 +0900

    [손현지] flutter 4강 - sns 마이페이지 만들기

---
+++ hyeonjiSon/flutter_repeat/fourth/lib/components/AppTheme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme () {
+  return ThemeData(
+      primaryColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.white)
+      )
+  );
+}
---
+++ hyeonjiSon/flutter_repeat/fourth/lib/components/ProfileButtons.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileButtons extends StatelessWidget {
+  const ProfileButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _buildFollowButton(),
+        _buildMessageButton(),
+      ],
+    );
+  }
+
+  Widget _buildFollowButton() {
+    return InkWell(
+      onTap: () {
+        print("팔로우 버튼 클릭");
+      },
+      child: Container(
+          alignment: Alignment.center,
+          width: 150,
+          height: 45,
+          child: const Text("팔로우", style: TextStyle(color: Colors.white)),
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(10)
+          )
+      ),
+    );
+  }
+
+  Widget _buildMessageButton() {
+    return InkWell(
+      onTap: () {
+        print("메시지 보내기 버튼 클릭");
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: 150,
+        height: 45,
+        child: const Text("메시지", style: TextStyle(color: Colors.black)),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all()
+        ),
+      ),
+    );
+  }
+}
